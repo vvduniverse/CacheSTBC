c5db0ef891598df057a624c08b8275b3626980fd
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-06 13:16:33 +0300

    Initial Commit

---
+++ CacheSTBC/CacheSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheSTBCApp.swift
+//  CacheSTBC
+//
+//  Created by Vahtee Boo on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CacheSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
